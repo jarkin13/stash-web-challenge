57bc6a1b3bd3f2072f2e611ff2404a6bda2fbab9
Jamie Arkin <jamielaurenarkin@gmail.com>
2017-10-02 20:39:02 -0400

    initial giphy api request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      endpoint: 'trending',
+      offset: 0,
+      gifs: []
+    }
+  }
+
+  getGifs() {
+    const PUBLIC_KEY = 'GZKGwdu6xlIM0iV58yFKJOFLqj0NLXFw';
+    const BASE_URL = '//api.giphy.com/v1/gifs/';
+    const LIMIT = 20;
+
+    $.ajax({
+      url: `${BASE_URL}${this.state.endpoint}?q=${this.state.text}&offset=${this.state.offset}&limit=${LIMIT}&api_key=${PUBLIC_KEY}`,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({gifs: data.data}, function() {
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+      }
+    });
+  }
+
+  componentWillMount() {
+    this.getGifs();
+  }
+
+  componentDidMount() {
+    this.getGifs();
+  }
+
   render() {
     return (
       <div className="App">
